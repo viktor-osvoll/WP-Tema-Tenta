a7e4dbface2b9601f84bd748bad59ad6a5d2ec5e
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-05-18 16:51:41 +0200

    added archive for cats and its looping out every cat post

--- archive-cats.php
+++ archive-cats.php
@@ -1,15 +1,30 @@
 <? get_header(); ?>
 
-<div class="container main-content clearfix">
-    <div class="left">
         <h1>All cats up for adoption</h1>
-        <? if(have_posts()) : while(have_posts()) : the_post(); ?>
-            <div class="cat">
-                <h2><a href="<? the_permalink(); ?>"><? the_title(); ?></a></h2>
-                <p><? the_field('age'); ?></p>
+       <? 
+       $args = array('post_type' => 'cats');
+       $query = new WP_Query($args);?>
+
+    <div class="container">
+        <div class="row">
+     <?  while($query->have_posts()) : $query->the_post();  ?>
+
+            <div class="card" style="width: 18rem;">
+           <?
+           $image = get_field('cat_image');
+           if(!empty($image) ) : ?>
+           <img src="<? echo esc_url($image['url']); ?>" alt="<? echo esc_attr($image['alt']); ?>">
+           <? endif; ?>
+           
+  <div class="card-body">
+    <h5 class="card-title"><? the_title(); ?></h5>
+    <p class="card-text"><? the_field('cat_gender'); ?></p>
+    <a href="<? the_permalink(); ?>" class="btn btn-primary">View Cat</a>
+  </div>
+</div>
             </div>
        <? endwhile; ?>
-     <? endif; ?>
+<? wp_reset_postdata(); ?>
     </div>
 </div>
 
