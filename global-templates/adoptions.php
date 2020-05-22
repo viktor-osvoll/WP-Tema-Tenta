bf13652591ebbd82775f8eb18b7cde050f8a5ab1
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-05-23 00:44:48 +0200

    added some css to adoption-usps

--- global-templates/adoptions.php
+++ global-templates/adoptions.php
@@ -8,17 +8,27 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
+  <div class="adopt">
+            <h1 class="adopt-title">How to adopt</h1>
+        </div>
 
 <? $adopting = new WP_Query([
     'post_type'     => 'adoptions',
     'order'         => 'ASC',
 ]); ?>
 
+<div class="container">
+    <div class="row">
+
 <? while($adopting->have_posts()) : $adopting->the_post(); ?>
-    <div class="zone">
-        <? the_title(); ?> <br>
-        <? the_field('how_to_adopt'); ?> <br>
+    <div class="card col-3 col-md-3 col-lg-3 mx-auto adoption-card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title"><? the_title(); ?></h5>
+                       <hr>
+            <p class="card-text"><? the_field('how_to_adopt'); ?></p>
+        </div>
     </div>
 <? endwhile; ?>
-
+  </div> <!-- /row -->
+</div><!-- /container -->
 <? wp_reset_postdata(); ?>
--- loop-templates/content-success_stories.php
+++ loop-templates/content-success_stories.php
@@ -12,5 +12,6 @@ defined( 'ABSPATH' ) || exit;
   <div class="media-body media-text">
     <h5 class="mt-0"><? the_title(); ?></h5>
     <p><? the_field('success_story'); ?></p>
+    <footer class="blockquote-footer"> <cite title="Source Title"><? the_author(); ?></cite></footer>
   </div>
 </div>
