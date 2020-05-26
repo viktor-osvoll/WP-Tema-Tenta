513262b2690fbf98126f8be6d98f51875a73ce5e
viktor-osvoll <viktor.osvoll@medieinstitutet.se>
2020-05-26 12:57:00 +0200

    fixed some syntax

--- functions.php
+++ functions.php
@@ -17,13 +17,14 @@ $understrap_includes = array(
 	'/pagination.php',                      // Custom pagination for this theme.
 	'/hooks.php',                           // Custom hooks.
 	'/extras.php',                          // Custom functions that act independently of the theme templates.
-	'/customizer.php',                      // Customizer additions.
-	'/custom-comments.php',                 // Custom Comments file.
 	'/custom-header.php',					// Load Custom Header,
 	'/custom-post-types.php',				// Load Custom Post Types.
 	'/custom-taxonomies.php',				// Load Custom Taxonomies.
 	'/acf.php',								// Load Plugin.
+	'/acf-fieldgroups.php',					// Load Custom Field Groups.
 	'/acf-options.php',						// Register ACF Options Page.
+	'/customizer.php',                      // Customizer additions.
+	'/custom-comments.php',                 // Custom Comments file.
 	'/jetpack.php',                         // Load Jetpack compatibility file.
 	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
 	'/woocommerce.php',                     // Load WooCommerce functions.
